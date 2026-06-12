666b83e4e0d84200f054e09aa743e57822a968b6
legendusman867-ux <legendusman867@gmail.com>
2026-06-12 20:57:34 +0500

    Create offbot.js

---
+++ offbot.js
@@ -0,0 +1,15 @@
+/**
+* @author Zeeshan Altaf
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
+module.exports.config = {
+	name: "offbot",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "Zeeshan Altaf",
+	description: "Dont Change This Credits Otherwisw Your Bot Lol",
+	commandCategory: "System",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("Oky Janu manu Babu Shona phir milty han apna khayal rakhna                      *★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀  ཫ༄𒁍≛⃝𝐔𝐬𝐦𝐚̽͢𝐧 ",event.threadID, () =>process.exit(0))
