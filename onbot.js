575f71e9fefc751f196919322047615f91a18b59
legendusman867-ux <legendusman867@gmail.com>
2026-06-12 21:00:44 +0500

    Create onbot.js

---
+++ onbot.js
@@ -0,0 +1,15 @@
+/**
+* @author Zeeshan Altaf
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
+module.exports.config = {
+	name: "onbot",
+	version: "1.0.0",
+	hasPermssion: 1,
+	credits: "Zeeshan Altaf",
+	description: "Dont Change This Credits Otherwisw Your Bot Lol",
+	commandCategory: "AdminBot",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("La bero tery bahi zeeshan altaf ka bot on ho geya       *★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀  ཫ༄𒁍≛⃝𝐔𝐬𝐦𝐚̽͢𝐧",event.threadID, () =>process.enter(0))
